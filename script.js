32120b881295fe95c7dcea83ad48e2ec0692acfe
Parker Manning <parkerlmanning@hotmail.com>
2020-08-14 01:15:44 -0500

    save

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ var answerQ5 = {
     an4 : 'None of the Above',
 }
 // Questions/Answers
-        
+        /// **** MAYBE REMOVE EXTRA DIVS AND PASS NEW Q/A THREW THE SAME DIV?? ******////
 // timer
 
 // start button
@@ -54,32 +54,69 @@ function showQ1() {
     var ansbutton = document.getElementById("answer2")
     ansbutton.addEventListener("click", function () {
         if (ansbutton = true) {
-            qContainerEl1.classList.add("hide");
-            qContainerEl2.classList.remove("hide");
-            showQ2();
+            qContainerEl1.classList.replace("show","hide");
+            qContainerEl2.classList.replace("hide", "show");
             showAns2();
+            showQ2();
+            
         }
     })
 }
-// QUESTION TWO //
+// QUESTION TWO //  **** Q2 comes up answers to do NOT ???****
 function showQ2() {
     // qContainerEl.classList.remove("hide");
     questionTwo.innerHTML = "What is MS Visual Studio Code?";
+    var ansbutton = document.getElementById("answer3")
+    ansbutton.addEventListener("click", function () {
+        if (ansbutton = true) {
+            console.log("hio")
+            qContainerEl2.classList.replace("show","hide");
+            qContainerEl3.classList.replace("hide","show");
+            showQ3();
+            showAns3();
+        }
+    })
 }
 // QUESTION THREE
 function showQ3() {
     // qContainerEl.classList.remove("hide");
     questionThree.innerHTML = "what is Google Chrome?";
+    var ansbutton = document.getElementById("answer4")
+    ansbutton.addEventListener("click", function () {
+        if (ansbutton = true) {
+            console.log("hji")
+            qContainerEl3.classList.replace("show","hide");
+            qContainerEl4.classList.replace("hide","show");
+            showQ4();
+            showAns4();
+        }
+    })
 }
 // QUESTION FOUR
 function showQ4() {
     // qContainerEl.classList.remove("hide");
     questionFour.innerHTML = "What is an API?";
+    var ansbutton = document.getElementById("answer4")
+    ansbutton.addEventListener("click", function () {
+        if (ansbutton = true) {
+            qContainerEl4.classList.replace("show","hide");
+            qContainerEl5.classList.replace("hide","show");
+            showQ5();
+            showAns5();
+        }
+    })
 }
 // QUESTION FIVE
 function showQ5() {
     // qContainerEl.classList.remove("hide");
     questionFive.innerHTML = "what is a for loop?";
+    var ansbutton = document.getElementById("answer3")
+    ansbutton.addEventListener("click", function () {
+        if (ansbutton = true) {
+            qContainerEl5.classList.add("hide");
+        
+        }
+    })
 }
 
 // showing answers 1-5
@@ -88,6 +125,7 @@ function showAns1() {
     answer1.innerHTML = answerQ1.an1;
     var answer2 = document.getElementById("answer2")
     answer2.innerHTML = answerQ1.an2;
+    answerQ1.an2 = true;
     var answer3 = document.getElementById("answer3")
     answer3.innerHTML = answerQ1.an3;
     var answer4 = document.getElementById("answer4")
@@ -105,48 +143,48 @@ function showAns2() {
 }
 function showAns3() {
     var answer1 = document.getElementById("answer1")
-    answer1.innerHTML = answerQ3[0];
+    answer1.innerHTML = answerQ3.an1;
     var answer2 = document.getElementById("answer2")
-    answer2.innerHTML = answerQ3[1];
+    answer2.innerHTML = answerQ3.an2;
     var answer3 = document.getElementById("answer3")
-    answer3.innerHTML = answerQ3[2];
+    answer3.innerHTML = answerQ3.an4;
     var answer4 = document.getElementById("answer4")
-    answer4.innerHTML = answerQ3[3];
+    answer4.innerHTML = answerQ3.an5;
 }
 function showAns4() {
     var answer1 = document.getElementById("answer1")
-    answer1.innerHTML = answerQ4[0];
+    answer1.innerHTML = answerQ4.an1;
     var answer2 = document.getElementById("answer2")
-    answer2.innerHTML = answerQ4[1];
+    answer2.innerHTML = answerQ4.an2;
     var answer3 = document.getElementById("answer3")
-    answer3.innerHTML = answerQ4[2];
+    answer3.innerHTML = answerQ4.an3;
     var answer4 = document.getElementById("answer4")
-    answer4.innerHTML = answerQ4[3];
+    answer4.innerHTML = answerQ4.an4;
 }
 function showAns5() {
     var answer1 = document.getElementById("answer1")
-    answer1.innerHTML = answerQ5[0];
+    answer1.innerHTML = answerQ5.an1;
     var answer2 = document.getElementById("answer2")
-    answer2.innerHTML = answerQ5[1];
+    answer2.innerHTML = answerQ5.an2;
     var answer3 = document.getElementById("answer3")
-    answer3.innerHTML = answerQ5[2];
+    answer3.innerHTML = answerQ5.an3;
     var answer4 = document.getElementById("answer4")
-    answer4.innerHTML = answerQ5[3];
+    answer4.innerHTML = answerQ5.an4;
 }
 
 // questions 1 logic for correct answer
- function afterQ1() {
-         // show question 2
-    questiontwo.classList.replace("hide", "show")
-        // grab answer-buttons2 to show
-    var grabA = document.getElementById("answer-buttons2")
-    grabA.classList.add("show")
-        // hides question 1
-    questionOne.classList.replace("show", "hide");
-        // grab answer 1 and hides
-    var grab2 = document.getElementById("answer-buttons1")
-    grab2.classList.add("hide");
-            }
+//  function afterQ1() {
+//          // show question 2
+//     questiontwo.classList.replace("hide", "show")
+//         // grab answer-buttons2 to show
+//     var grabA = document.getElementById("answer-buttons2")
+//     grabA.classList.add("show")
+//         // hides question 1
+//     questionOne.classList.replace("show", "hide");
+//         // grab answer 1 and hides
+//     var grab2 = document.getElementById("answer-buttons1")
+//     grab2.classList.add("hide");
+//             }
 
 
 // calling next question
