566a7dfd82e6a13fe822a3c66c1341c1e7f13e2c
Parker Manning <parkerlmanning@hotmail.com>
2020-09-23 03:21:10 -0500

    added function for inserting the answers

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const questions = [
                 "A random acronym",
                 "A dog",
                 "A robot",
-            ]
+            ],
+        correct: "A coding language"
     },
     {
         question: "What is a Function?",
@@ -19,7 +20,8 @@ const questions = [
                 "A popular song name",
                 "A useful tool in javascript",
                 "A computer",
-            ]
+            ],
+        correct: "a useful tool in javascript"
     },
     {
         question: "What is a laptop",
@@ -29,7 +31,8 @@ const questions = [
                 "A computer designed to sit on your lap",
                 "A skateboard",
                 "A robot",
-            ]
+            ],
+        correct: "A computer designed to sit on your lap"
     },
     {
         question: "What is CSS",
@@ -39,16 +42,32 @@ const questions = [
                 "Popular name",
                 "Type of Doggo",
                 "Cascading Style Sheets",
-            ]
+            ],
+        correct: "Cascading Style Sheets"
     },
 ];
-// console.table(questions);
+ console.table(questions);
 
 // Onclick Listener for the start button
 
 $(".start").on("click", () => {
     $(".start").addClass("w3-hide")
     // startGame();
-    });
+});
+    
+// creating function for inserting answers
+function insertAnswer() {
+    for (i = 0; i < 4; i++){
+        $(".answer" + i).text(questions[0].answers[i])
+    }
+}
 
 
+const startGame = function () {
+    
+    
+    
+    
+}
+startGame();
+insertAnswer()
